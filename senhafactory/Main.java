5baeb4a9d2ce989aef107ae8618994f0724ad372
Amaro Miranda <54691195+NagatoUx0@users.noreply.github.com>
2019-10-08 14:54:21 -0300

    Update Main.java

--- senhafactory/Main.java
+++ senhafactory/Main.java
@@ -1,1 +1,20 @@
-package senhafactory;import javax.swing.JOptionPane;/** * @author 		Ermerson Rafael - 816122707 * @category	Aplicações e Serviços * @since		21/02/2018 * @version		v1.0.0.4 */public class Main {	public static void main(String[] args) {		String senha = JOptionPane.showInputDialog(null, "Digite a senha:");		if (senha.isEmpty()) {			System.out.println("Digite uma senha...");			System.exit(1);		}		ProvedorInformacao provedor = ("designpatterns".equals(senha)) ? new ProvedorConfidencial()				: new ProvedorPublico();		provedor.exibirInformacao();	}}
+package senhafactory;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String senha = JOptionPane.showInputDialog(null, "Digite a senha:");
+
+		if (senha.isEmpty()) {
+			System.out.println("Digite uma senha...");
+			System.exit(1);
+		}
+
+		ProvedorInformacao provedor = ("designpatterns".equals(senha)) ? new ProvedorConfidencial()
+				: new ProvedorPublico();
+
+		provedor.exibirInformacao();
+	}
+}
