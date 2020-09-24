02ad7f13289903e7c211f11e765e0ea4b6150516
Amaro Miranda <54691195+NagatoUx0@users.noreply.github.com>
2020-09-24 19:50:04 -0300

    Add files via upload

---
+++ amaro-distribuidora/src/main/java/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@GetMapping
+	public String teste() {
+		
+		return "Estou sendo chamado pelo método GET";
+	}
+
+}
---
+++ amaro-distribuidora/src/main/java/modelo/Endereco.java
@@ -0,0 +1,54 @@
+package modelo;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name="tb_endereco")
+public class Endereco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	public String rua;
+	public String bairro;
+	public String cidade;
+	
+	public Endereco(String rua, String bairro, String cidade) {
+		super();
+		this.rua = rua;
+		this.bairro = bairro;
+		this.cidade = cidade;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+	
+}
